b9ee2b4c4dfda808128dc5de136df1736308f046
David Patterson <David Patterson>
2017-03-30 15:36:31 -0700

    first commit

---
+++ src/app/app.service.ts
@@ -0,0 +1,23 @@
+import {Component, Injectable} from '@angular/core';
+
+@Injectable()
+export class AppService{
+
+    //this is where variables go
+
+    constructor(){
+
+    }
+
+
+
+
+
+
+    toggle(prop:boolean){
+        console.log()
+    }
+
+    //this is where the function goes..
+
+}
